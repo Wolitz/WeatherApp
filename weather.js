fbb5f00a6ac7605cf4e84f353335a039522ada7b
MarkoVolic <markovolic16@gmail.com>
2023-07-19 19:47:12 +0200

    skoro pa gotovo

--- weather.js
+++ weather.js
@@ -1,6 +1,25 @@
+var city= "Beograd"
 
 function myFunction() {
-    var x = document.getElementById("myText").value;
-    document.getElementById("demo").innerHTML = x;
-    document.getElementById("myInput").value = '';
+     city = document.getElementById("myText").value;
+    console.log(city);
+
+    checkWeather();
   }
+
+
+const apiKey = "ffcc3b1d6839acddfd18b10b824cdae1";
+const apiUrl=  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + city +'&appid=' + apiKey);
+    var data = await response.json();
+    
+    document.querySelector("#demo").innerHTML = city;
+    document.querySelector(".temp").innerHTML = data.main.temp + " °C";
+    document.querySelector(".humidity").innerHTML = "H: " + data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML =  "WS: " + data.wind.speed + " KM/H";
+    const niz = data.weather;
+    console.log(niz);
+}
+checkWeather();
